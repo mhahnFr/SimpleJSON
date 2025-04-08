177be4df11e978c051c0c64c7ca4fb9c76fdb80d
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-04-08 12:46:54 +0200

    Documented the `Object`

--- include/SimpleJSON/Object.hpp
+++ include/SimpleJSON/Object.hpp
@@ -28,14 +28,46 @@
 #include "Value.hpp"
 
 namespace simple_json {
+/**
+ * @brief Represents a JSON object.
+ *
+ * It can be constructed from JSON values (if they contain an object) or from
+ * the raw content of a JSON object (the `ObjectContent` type).
+ *
+ * Various accessors enable the simple usage of the content of an JSON object.
+ */
 struct Object {
+    /** The raw content of this object. */
     ObjectContent content;
 
     Object() = default;
 
+    /**
+     * Constructs a JSON object from the given raw JSON object content.
+     *
+     * @param content the raw JSON content
+     */
     inline Object(const ObjectContent& content): content(content) {}
+
+    /**
+     * @brief Constructs a JSON object from the given JSON value.
+     *
+     * Will throw an exception if the given value does not contain a JSON object.
+     *
+     * @param value the JSON value
+     */
     inline Object(const Value& value): Object(value.as<ValueType::Object>()) {}
 
+    /**
+     * @brief Gets the contained value for the given key.
+     *
+     * Throws an exception if the value is found but the type does not match
+     * the given type.
+     *
+     * @param name the key whose mapped value to get
+     * @tparam T the type of the contained value
+     * @return the value for the given key or `std::nullopt` if not found or `null`
+     */
     template<typename T>
     constexpr inline auto get(const std::string& name) const -> std::optional<T> {
         const auto& it = content.find(name);
@@ -45,11 +77,29 @@ struct Object {
         return std::nullopt;
     }
 
+    /**
+     * @brief Gets the contained value for the given key.
+     *
+     * Throws an exception if the value is found but the type does not match
+     * the given type.
+     *
+     * @param name the key whose mapped value to get
+     * @tparam T the value type enumeration value of the desired type
+     * @return the value for the given key or `std::nullopt` if not found or @c null
+     */
     template<ValueType T>
     constexpr inline auto get(const std::string& name) const {
         return get<typename Trait<T>::Type>(name);
     }
 
+    /**
+     * @brief Gets the contained object for the given key.
+     *
+     * Throws an exception if the value  is found but is not a JSON object.
+     *
+     * @param name the key whose mapped object to get
+     * @return the object for the given key or `std::nullopt` if not found or @c null
+     */
     inline auto getObject(const std::string& name) const -> std::optional<Object> {
         if (auto object = get<ValueType::Object>(name)) {
             return Object { *object };
@@ -57,20 +107,56 @@ struct Object {
         return std::nullopt;
     }
 
+    /**
+     * @brief Returns a reference to the value referred to by the given key.
+     *
+     * Throws an exception if the value is not found or the type does not match.
+     *
+     * @param name the key whose mapped value to get
+     * @tparam T the type of the contained value
+     * @return the mapped value
+     */
     template<typename T>
     constexpr inline auto getBang(const std::string& name) const -> const T& {
         return std::get<T>(content.at(name).value);
     }
 
+    /**
+     * @brief Returns a reference to the value referred to by the given key.
+     *
+     * Throws an exception if the value is not found or the type does not match.
+     *
+     * @param name the key whose mapped value to get
+     * @tparam T the value type enumeration value of the desired type
+     * @return the mapped value
+     */
     template<ValueType T>
     constexpr inline auto getBang(const std::string& name) const -> const typename Trait<T>::Type& {
         return getBang<typename Trait<T>::Type>(name);
     }
 
+    /**
+     * @brief Gets the object referred to by the given key.
+     *
+     * Throws an exception if the object is not found or is not a JSON object.
+     *
+     * @param name the key whose mapped object to get
+     * @return the mapped object
+     */
     inline auto getObjectBang(const std::string& name) const -> Object {
         return Object { getBang<ValueType::Object>(name) };
     }
 
+    /**
+     * @brief Gets the object referred to by the given key and casts it to the
+     * given type.
+     *
+     * Uses the functional style cast.
+     *
+     * @param name the key whose mapped object to get
+     * @tparam T the type to construct from the contained JSON object
+     * @return the mapped object casted to the given type or `std::nullopt` if not found or @c null
+     */
     template<
 #if __cplusplus >= 202002L
     std::constructible_from<Object>
